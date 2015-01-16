8939cd329dc9e0c0e9f5bb7ebe4749b0a4d0f8a5
Andrea Marco Sartori <22cerbero22@gmail.com>
2015-01-16 17:00:06 +0100

    Create package

---
+++ src/AffiliateServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Cerbero\Affiliate;
+
+use Illuminate\Support\ServiceProvider;
+
+class AffiliateServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('cerbero/affiliate');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
